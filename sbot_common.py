66254166c2324b74b573b85513cb4bb210cd7789
cepthomas <cepthomas@gmail.com>
2022-04-14 12:16:04 -0500

    Working.

--- sbot_common.py
+++ sbot_common.py
@@ -1,23 +1,28 @@
 import os
 import subprocess
+import pathlib
 import sublime
 import sublime_plugin
 
 
-
-# In signet/highlight
-# Decorator for tracing function entry.
-def trace_func(func):
+def trace_method(method):
+    ''' Decorator for tracing method entry. '''
     def inner(ref, *args):
-        print(f'FUN {ref.__class__.__name__}.{func.__name__} {args}')
-        return func(ref, *args)
+        print(f'MET {ref.__class__.__name__}.{method.__name__} {args}')
+        return method(ref, *args)
     return inner
 
 
-# In signet/highlight _get_store_fn + , file_ext):
+def trace_function(func):
+    ''' Decorator for tracing function entry. '''
+    def inner(*args):
+        print(f'FUN {func.__name__} {args}')
+        return func(*args)
+    return inner
+
+
 def get_store_fn(project_fn, file_ext):
-    ''' General utility. '''
-    
+    ''' General utility to get store file name. '''
     store_path = os.path.join(sublime.packages_path(), 'User', 'SbotStore')
     pathlib.Path(store_path).mkdir(parents=True, exist_ok=True)
     project_fn = os.path.basename(project_fn).replace('.sublime-project', file_ext)
@@ -25,23 +30,30 @@ def get_store_fn(project_fn, file_ext):
     return store_fn
 
 
-# In clean/format/render _get_sel_regions + , mod_name) 'SbotClean'
-def get_sel_regions(view, mod_name):
+def get_sel_regions(view, settings):
     ''' Function to get selections or optionally the whole view if sel_all is True.'''
     regions = []
     if len(view.sel()[0]) > 0:  # user sel
         regions = view.sel()
     else:
-        settings = sublime.load_settings(f"{mod_name}.sublime-settings")
         if settings.get('sel_all'):
             regions = [sublime.Region(0, view.size())]
     return regions
 
 
-# In format/sidebar _create_new_view
 def create_new_view(window, text):
     ''' Creates a temp view with text. Returns the view.'''
     vnew = window.new_file()
     vnew.set_scratch(True)
     vnew.run_command('append', {'characters': text})  # insert has some odd behavior - indentation
     return vnew
+
+
+#-----------------------------------------------------------------------------------
+class SbotGeneralEvent(sublime_plugin.EventListener):
+    ''' Listener for window events of interest. '''
+
+    def on_selection_modified(self, view):
+        ''' Show the abs position in the status bar. '''
+        pos = view.sel()[0].begin()
+        view.set_status("position", f'Pos {pos}')
